6385bc49ca0263f20af47ff0a0e8bf29b0db0b4d
Bibek Thapa <bibek.thapa619@gmail.com>
2020-06-09 14:55:23 +0545

    Add query and mutation

--- schema.js
+++ schema.js
@@ -21,3 +21,74 @@ let directors = [
   { id: "3", name: "Kiran Parajuli" },
 ];
 
+const MovieType = new GraphQLObjectType({
+  name: "movie",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    directorID: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return _.find(directors, { id: parent.directorID });
+      },
+    },
+  }),
+});
+
+const MovieQuery = {
+  type: MovieType,
+  args: { id: { type: GraphQLID } },
+  resolve(parent, args) {
+    return _.find(movies, { id: args.id });
+  },
+};
+
+const DirectorType = new GraphQLObjectType({
+  name: "director",
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    movies: { type: GraphQLList(MovieType), resolve(parent, args) {
+      return _.filter(movies,{directorID:parent.id})
+    } },
+  },
+});
+
+const DirectorQuery = {
+  type: DirectorType,
+  args: { id: { type: GraphQLID } },
+  resolve(parent, args) {
+    return _.find(directors, { id: args.id });
+  },
+};
+
+const RootQuery = new GraphQLObjectType({
+  name: "Rootquery",
+  fields: {
+    movie: MovieQuery,
+    director: DirectorQuery,
+  },
+});
+
+const addDirectorMutation = {
+  type: DirectorType,
+  args:{id:{type:GraphQLID}, name:{type:GraphQLString}},
+  resolve(parent,args)
+  {
+    directors.push(args);
+    console.log(directors);
+    return args;
+  }
+}
+
+const RootMutation = new GraphQLObjectType({
+  name:"mutations",
+  fields:{ addDirector : addDirectorMutation}
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation:RootMutation
+});
