6d6e7bb96d66847f34883513761bba6d06d024d3
Dipak Acharya <dpakach@gmail.com>
2020-06-22 20:50:45 +0545

    Query with GraphQl

--- schema.js
+++ schema.js
@@ -21,3 +21,61 @@ let directors = [
   { id: "3", name: "Kiran Parajuli" },
 ];
 
+const movieType = new GraphQLObjectType({
+	name: "movie",
+	fields: () => ({
+		id: {type: GraphQLID},
+		name: {type: GraphQLString},
+		genre: {type: GraphQLString},
+		directorID: {type: GraphQLString},
+		director: {
+			type: directorType,
+			resolve(parent) {
+				return _.find(directors, {id: parent.directorID})
+			}
+		}
+	})
+})
+
+const movieResolver = {
+	type: movieType,
+	args: {id: {type: GraphQLID}},
+	resolve(parent, args, context, info) {
+		return _.find(movies, {id: args.id});
+	}
+}
+
+const directorType = new GraphQLObjectType({
+	name: "director",
+	fields: {
+		id: {type: GraphQLID},
+		name: {type: GraphQLString},
+		movie: {
+			type: GraphQLList(movieType),
+			resolve(parent, args) {
+				return _.filter(movies, {directorID: parent.id})
+			}
+		}
+	}
+})
+
+const directorResolver = {
+	type: directorType,
+	args: {id: {type: GraphQLID}},
+	resolve(parent, args) {
+		return _.find(directors, {id: args.id});
+	}
+}
+
+const RootQuery = new GraphQLObjectType({
+	name: "rootquery",
+	fields: {
+		movie: movieResolver,
+		director: directorResolver
+	}
+})
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+})
+
